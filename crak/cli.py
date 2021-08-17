b67b666605a33fe1b68fbb8c9aafe3084d7cc1bf
Stephen Eaton <seaton@strobotics.com.au>
2021-08-17 12:42:19 +0800

    updated LICENSE updaded package meta bumped version

--- crak/cli.py
+++ crak/cli.py
@@ -13,7 +13,7 @@ import argparse
 import pandas as pd
 from passlib.hosts import linux_context
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 # get and parse command line arguments
 PARSER = argparse.ArgumentParser(
